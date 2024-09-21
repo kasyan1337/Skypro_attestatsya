e6d95ff3e3c900afa31f75a50da6402acad00b0e
kasyan1337 <tristokorun@gmail.com>
2024-09-21 18:14:03 +0200

    permissions.py IsActiveStaff

--- network/views.py
+++ network/views.py
@@ -3,6 +3,7 @@ from rest_framework import permissions, viewsets
 
 from network.models import NetworkNode
 from network.serializers import NetworkNodeSerializer
+from .permissions import IsActiveStaff
 
 
 # Create your views here.
@@ -10,7 +11,7 @@ from network.serializers import NetworkNodeSerializer
 class NetworkNodeViewSet(viewsets.ModelViewSet):
     queryset = NetworkNode.objects.all()
     serializer_class = NetworkNodeSerializer
-    permission_classes = [permissions.IsAuthenticated]
+    permission_classes = [IsActiveStaff]
     filter_backends = [DjangoFilterBackend]
     filterset_fields = ['country']  # enables filtering via query parameters
 
